52f6058acdbbda794b7908627506c6be6354369f
Ss_Tr33s <24_de_7@protonmail.com>
2023-03-13 09:54:50 -0500

    testeo básico

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -25,6 +26,33 @@ class ExampleTest extends TestCase
         Artisan::call('migrate');
 
         $carga=$this->get(route('registro'));
-        $carga->assertStatus(200);
+        $carga->assertStatus(200)->assertSee("register");
+
+        //Registro corecto
+
+        $registerBien=$this->post(route('validar-registro',["name"=>"test","email" => "aaaa7@gmail.com","password"=>"123"]));
+
+        $registerBien->assertStatus(302)->assertRedirect(route('privada'));
+
+        $this->assertDatabaseHas('users',["name"=>"test","email" => "aaaa@gmail.com"]);
+
     }
+
+    public function test_login(): void{
+        //Artisan::call('migrate');
+
+        $carga=$this->get(route('login'));
+        $carga->assertStatus(200)->assertSee("login");
+
+        //Registro corecto
+
+        $loginBien=$this->post(route('inicia-session',
+        ["name"=>"Andre Wisoky","email" => "moore.kamryn@example.com","password"=>"password"]));
+
+        $loginBien->assertStatus(302)->assertRedirect(route('privada'));
+
+        $carga=$this->get(route('logout'));
+        $carga->assertStatus(302)->assertRedirect(route('login'));
+    }
+
 }
